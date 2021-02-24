a652315a8e2263727eb3145a4c482ad051d649a4
Orlando Flores <orlando_flores@id.uff.br>
2021-02-24 18:53:14 -0300

    exercicio 2

---
+++ bloco4/dia1/exercises.js
@@ -0,0 +1,18 @@
+let a = 7;
+let b = 13;
+
+let soma = a+b;
+let subtracao = a-b;
+let mult = a*b;
+let div = a/b;
+let mod = a%b;
+
+let num1 = 17;
+let num2 = 20;
+if (num1 > num2) {
+    console.log('num1 é maior');
+} else if (num1 == num2) {
+    console.log('numeros iguais');
+} else {
+    console.log('num2 é maior');
+}
