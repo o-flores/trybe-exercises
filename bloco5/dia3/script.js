a6de0bd538480e29604ff0297775e46f0e6e957a
Orlando Flores <orlando_flores@id.uff.br>
2021-03-05 19:39:03 -0300

    Exercicio 1

--- bloco5/dia3/script.js
+++ bloco5/dia3/script.js
@@ -14,3 +14,26 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+
+  function createDaysOfTheMonth() {
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const daysNumbers = document.getElementById('days');
+
+    for (let i=0; i < dezDaysList.length; i+=1) {
+        const number = dezDaysList[i];
+        const dayNumber = document.createElement('li');
+        dayNumber.innerText = number;
+        dayNumber.className = 'day';
+
+        daysNumbers.appendChild(dayNumber);
+
+        if (dezDaysList[i] === 24 || dezDaysList[i] === 25 || dezDaysList[i] === 31) {
+            dayNumber.className += ' holiday';
+        }
+        if (dezDaysList[i] === 4 || dezDaysList[i] === 11 || dezDaysList[i] === 18 || dezDaysList[i] === 25) {
+            dayNumber.className += ' friday';
+        }
+    };
+  };
+
+  createDaysOfTheMonth();
