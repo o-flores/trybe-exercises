8fca08d910257f635944c507072e0b7cca0e37a4
Orlando Flores <orlando_flores@id.uff.br>
2021-03-23 19:03:10 -0300

    parte 2 - exercicio 1 usando ternary operator

--- bloco7/dia1/exercises.js
+++ bloco7/dia1/exercises.js
@@ -1,9 +1,3 @@
-const factorial = (number) => {
-  let result = 1;
-  for (let index = 2; index <= number; index += 1){
-    result *= index;
-  }
-  return result
-}
+const factorial = (number) => number > 1 ? number * factorial(number - 1) : 1
 
-console.log(factorial(4));
+console.log(factorial(0))
