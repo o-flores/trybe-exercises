38fac38c11110afbc5740e77e974aa1cb9ed46ae
Orlando Flores <orlando_flores@id.uff.br>
2021-03-23 19:31:16 -0300

    parte 2 exercicio 2

--- bloco7/dia1/exercises.js
+++ bloco7/dia1/exercises.js
@@ -1,3 +1,20 @@
-const factorial = (number) => number > 1 ? number * factorial(number - 1) : 1
+const longestWord = (phrase) => {
+  const words = phrase.split(' ');
+  let count =[];
+  for (let index = 0; index < words.length; index +=1) {
+    count.push(words[index].length);
+  }
+  return words[indexOfHigherNumber(words)];
+}
 
-console.log(factorial(0))
+const indexOfHigherNumber = (array) => {
+  let higher = array[0];
+  for (let index = 1; index < array.length; index += 1) {
+    if (array[index] > higher) {
+      higher = array[index];
+    }
+  }
+  let number = array.indexOf(higher) - 1;
+  return number;
+}
+console.log(longestWord('A maior palavra é esta aqui'))
