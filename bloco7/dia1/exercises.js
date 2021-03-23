efb491a0d9c7211a1c5c04da6f56da0b982efb5f
Orlando Flores <orlando_flores@id.uff.br>
2021-03-23 18:48:33 -0300

    exercicio 2

--- bloco7/dia1/exercises.js
+++ bloco7/dia1/exercises.js
@@ -1,12 +1,4 @@
-const testingScope = (escopo) => {
-    if (escopo === true) {
-      let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
-      ifScope = ifScope + ` ótimo, fui utilizada no escopo !`;
-      console.log(ifScope);
-    } else {
-      let elseScope = `Não devo ser utilizada fora meu escopo (else)`;
-      console.log(elseScope);
-    }
-  }
+const oddsAndEvensArray = [13, 3, 4, 10, 7, 2];
+const oddsAndEvens = (array) => array.sort(function(a,b){return a - b});
 
-  testingScope(true);
+console.log(oddsAndEvens(oddsAndEvensArray));
