fd8839ea0350839671f3142a901b86b95f5ca078
Orlando Flores <orlando_flores@id.uff.br>
2021-03-25 18:45:11 -0300

    Exercicios - parte 2

--- bloco7/dia3/exercises.js
+++ bloco7/dia3/exercises.js
@@ -10,4 +10,23 @@ function sum(a,b) {
 assert.strictEqual(sum(1,1),2);
 assert.strictEqual(sum(4,5),9);
 assert.strictEqual(sum(0,0),0);
-assert.strictEqual(sum(4,'5'),9);
+// assert.strictEqual(sum(4,'5'),9);
+
+function myRemove(arr, item) {
+  let result = [];
+  for (index of arr) {
+    if (index !== item) {
+      result.push(index);
+    }
+  }
+  return result;
+}
+
+assert.deepStrictEqual(myRemove([1,2,3,4],3),[1,2,4]);
+assert.notDeepStrictEqual(myRemove([1,2,3,4],3),[1,2,3,4]);
+
+const teste = [1,2,3,4,5];
+myRemove(teste,2);
+assert.deepStrictEqual(teste,[1,2,3,4,5]);
+
+assert.deepStrictEqual(myRemove([1,2,3,4], 5), [1,2,3,4]);
