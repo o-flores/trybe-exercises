ca6a0c1ff4e963b71c2bf359620fd6e138336c74
Orlando Flores <orlando_flores@id.uff.br>
2021-03-25 22:05:14 -0300

    bonus 4 usando o slice -para nao modificar o array original- e o sort que ja organiza o array em ordem decrescente

--- bloco7/dia3/bonus4.js
+++ bloco7/dia3/bonus4.js
@@ -1,16 +1,13 @@
 const getLargestNumber = (array) => {
-  let largestNumber = array[0];
-  for (let index = 1; index < array.length; index += 1) {
-      if (array[index] > largestNumber) {
-          largestNumber = array[index];
-      }
-  }
-  return largestNumber;
+  let result = array.slice().sort((a,b) => b - a)[0];
+  console.log(array)
+  return result;
 }
 
 const parameter = [45, 8, 2, 50, 1, 7, 99];
 const result = 99;
 
 const assert = require('assert');
+
 
 assert.strictEqual(getLargestNumber(parameter), result)
