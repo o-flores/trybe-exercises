c2afbde4a22785fdc0602af51d72703616a5c8f0
Orlando Flores <orlando_flores@id.uff.br>
2021-03-29 18:17:53 -0300

    exercicio 3

--- bloco8/dia1/exercises.js
+++ bloco8/dia1/exercises.js
@@ -1,15 +1,10 @@
-const generateRandomNumber = () => Math.round(Math.random()*5);
-
-const verifyNumber = (number1, number2) => {
-  if (number1 === number2) {
-    return `Parabéns você ganhou`;
-  }
-  return `Tente novamente`;
+const checkAnswer = (answer) => {
+  return validateUserAnswer = (userAnswer) => {
+    if (answer.toUpperCase() === userAnswer.toUpperCase()) {
+      return true;
+    }
+    return false;
+  };
 }
 
-const checkSorteio = (numero, verifyNumber) => {
-  let numeroSorteado = generateRandomNumber();
-  return verifyNumber(numero, numeroSorteado);
-}
-
-console.log(checkSorteio(4,verifyNumber));
+console.log(checkAnswer('teste')('TESTE'))
