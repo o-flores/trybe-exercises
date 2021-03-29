ae9bd0d95aa507c79c42c60ffb168f0a40a349b1
Orlando Flores <orlando_flores@id.uff.br>
2021-03-29 19:27:01 -0300

    exercicio 4

--- bloco8/dia1/exercises.js
+++ bloco8/dia1/exercises.js
@@ -1,10 +1,20 @@
-const checkAnswer = (answer) => {
-  return validateUserAnswer = (userAnswer) => {
-    if (answer.toUpperCase() === userAnswer.toUpperCase()) {
-      return true;
+const checkAnswers = (array1, array2) => {
+  let sum = 0;
+  for (let index = 0; index < array1.length; index += 1) {
+    if (array2[index] !== 'N.A') {
+      if (array1[index] === array2[index]) {
+        sum += 1;
+      } else {
+        sum -= 0.5;
+      }
     }
-    return false;
-  };
+  }
+  return sum;
 }
 
-console.log(checkAnswer('teste')('TESTE'))
+const checkTestAnswers = (rightAnswers, userAnswers, checkAnswers) => {
+  return checkAnswers(rightAnswers, userAnswers);
+}
+
+
+console.log(checkTestAnswers(['a','b','c','a'],['N.A','b','c','d'],checkAnswers));
