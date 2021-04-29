62a1bce269ede4e3fca33380fcfeda225ad68a8a
Orlando Flores <orlando_flores@id.uff.br>
2021-04-29 20:00:40 -0300

    exercicio 1

--- bloco12/dia1/src/App.js
+++ bloco12/dia1/src/App.js
@@ -4,12 +4,41 @@ import pokemons from './data';
 import Pokedex from './Pokedex';
 
 class App extends React.Component {
+  constructor() {
+
+    super();
+
+    this.state = {
+      index: 0,
+    }
+
+    this.changePokemon = this.changePokemon.bind(this);
+    this.verifyIndex = this.verifyIndex.bind(this);
+
+  }
+
+  
+
+  changePokemon() {
+    this.setState((initialState, _props) => ({
+      index: initialState.index + 1,
+    }))
+  }
+
+  verifyIndex(index) {
+    if (index === pokemons.length) {
+      this.setState({ index: 0 })
+      return 0;
+    } 
+    return index;
+  }
+
   render() {
     return (
       <div className="App">
         <h1> Pokedex </h1>
-        <Pokedex pokemons={pokemons} />
-        <button>Próximo</button>
+        <Pokedex pokemons={pokemons[this.verifyIndex(this.state.index)]} />
+        <button onClick={this.changePokemon}>Próximo</button>
       </div>
     );
   }
