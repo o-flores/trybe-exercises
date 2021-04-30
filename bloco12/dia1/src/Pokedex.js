003adea75272dea0b68decbb8c842153306bd6b9
Orlando Flores <orlando_flores@id.uff.br>
2021-04-29 23:25:02 -0300

    exercicio 2

--- bloco12/dia1/src/Pokedex.js
+++ bloco12/dia1/src/Pokedex.js
@@ -9,34 +9,77 @@ class Pokedex extends React.Component {
 
         this.state = {
             index: 0,
+            filterType: 'Fire',
         }
 
-        this.changePokemon = this.changePokemon.bind(this);
+        this.nextPokemon = this.nextPokemon.bind(this);
         this.verifyIndex = this.verifyIndex.bind(this);
-
+        this.setPokemonType = this.setPokemonType.bind(this);
+        this.FilterPokemonsByType = this.FilterPokemonsByType.bind(this);
     }
 
 
-
-    changePokemon() {
+    nextPokemon() {
         this.setState((initialState, _props) => ({
             index: initialState.index + 1,
+
         }))
     }
 
     verifyIndex(index) {
-        if (index === this.props.pokemons.length) {
+        if (index === this.FilterPokemonsByType().length) {
             this.setState({ index: 0 })
             return 0;
         }
         return index;
     }
 
+    filteredPokemon() {
+
+    }
+
+    setPokemonType(event) {
+        this.setState({
+            index: this.state.index,
+            filterType: event.target.innerText
+        })
+    }
+
+    FilterPokemonsByType() {
+        const { filterType } = this.state
+        return this.props.pokemons.filter((pokemon) => pokemon.type === filterType)
+    }
+
     render() {
+        const { verifyIndex, nextPokemon, setPokemonType, FilterPokemonsByType } = this;
+        const { pokemons } = this.props;
+        const { index, filterType } = this.state;
+
+        const pokemonTypes = pokemons.reduce((types, { type }) => [...types, type], [])
+
         return (
-            <div className="pokedex">
-                <Pokemon key={this.props.pokemons.id} pokemon={this.props.pokemons[this.verifyIndex(this.state.index)]} />
-                <button onClick={this.changePokemon}>Próximo</button>
+            <div>
+                <div className="pokedex">
+                    <Pokemon
+                        key={pokemons.id}
+                        pokemon={FilterPokemonsByType()[verifyIndex(index)]} />
+                </div>
+                <div>
+                    <button
+                        onClick={nextPokemon}>
+                        Próximo
+                    </button>
+
+                    <button
+                        onClick={setPokemonType}>
+                        Fire
+                        </button>
+
+                    <button
+                        onClick={setPokemonType}>
+                        Psychic
+                        </button>
+                </div>
             </div>
         );
     }
