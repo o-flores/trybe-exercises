e0181e331b095a38b99412f60155c8bb2e66ec5c
Orlando Flores <orlando_flores@id.uff.br>
2021-04-29 22:05:52 -0300

    passando a logica de renderizar um pokemon por vez para o componente Pokedex

--- bloco12/dia1/src/Pokedex.js
+++ bloco12/dia1/src/Pokedex.js
@@ -2,10 +2,41 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+
+    constructor() {
+
+        super();
+
+        this.state = {
+            index: 0,
+        }
+
+        this.changePokemon = this.changePokemon.bind(this);
+        this.verifyIndex = this.verifyIndex.bind(this);
+
+    }
+
+
+
+    changePokemon() {
+        this.setState((initialState, _props) => ({
+            index: initialState.index + 1,
+        }))
+    }
+
+    verifyIndex(index) {
+        if (index === this.props.pokemons.length) {
+            this.setState({ index: 0 })
+            return 0;
+        }
+        return index;
+    }
+
     render() {
         return (
             <div className="pokedex">
-                <Pokemon key={this.props.pokemons.id} pokemon={this.props.pokemons} />
+                <Pokemon key={this.props.pokemons.id} pokemon={this.props.pokemons[this.verifyIndex(this.state.index)]} />
+                <button onClick={this.changePokemon}>Próximo</button>
             </div>
         );
     }
