adcaca90f68bc010106dca13754390a1b59aca6a
Orlando Flores <orlando_flores@id.uff.br>
2021-05-06 19:40:41 -0300

    cria rotas dinamicas para informaçoes especificas de cada pokemon

--- exercise-pokedex-router/src/App.js
+++ exercise-pokedex-router/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
 import { BrowserRouter, Route } from 'react-router-dom'
+import PokemonDetails from './PokemonDetails';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="App">
         <h1> Pokedex </h1>
         <Route exact path="/" render={() => <Pokedex pokemons={pokemons}/>} />
+        <Route exact path="/:id" render={(props) => <PokemonDetails {...props} pokemons={pokemons} />}/>
       </div>
     </BrowserRouter>
   );
