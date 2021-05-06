10a33351039bfa46549f30a9e1e69c81a5d543e1
Orlando Flores <orlando_flores@id.uff.br>
2021-05-06 15:49:41 -0300

    tornando a pagina navegavel utilizando o react router dom

--- comprehension-exercises-router/src/App.js
+++ comprehension-exercises-router/src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Route exact path='/'><Home/></Route>
+        <Route path='/about'><About/></Route>
+        <Route path='/users'><Users/></Route>
+      </BrowserRouter>
     );
   }
 }
