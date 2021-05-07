adae2fb77ccdcf97bdb6b6357c7db3adda4f5da8
Orlando Flores <orlando_flores@id.uff.br>
2021-05-06 22:25:54 -0300

    adiciona algumas informaçoes ao comp  PokemonDetails

--- exercise-pokedex-router/src/PokemonDetails.js
+++ exercise-pokedex-router/src/PokemonDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Pokemon from './Pokemon';
+import PokemonLocations from './PokemonLocations';
 
 class PokemonDetails extends React.Component {
   render() {
@@ -11,6 +12,12 @@ class PokemonDetails extends React.Component {
         <h1>Pokemon Details</h1>
         <div className="pokedex">
           <Pokemon className="pokemon" pokemon={pokemon} />
+        </div>
+        <div className="summary">
+          <h2>Summary</h2>
+          <p>{ pokemon.summary}</p>
+          <h2>Pokemon Locations</h2>
+          <PokemonLocations locations={ pokemon.foundAt }/>
         </div>
       </div>
     )
