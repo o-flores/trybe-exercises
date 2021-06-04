1701e1511786d276f85409a62f06a81bea7be9e7
Orlando Flores <orlando_flores@id.uff.br>
2021-06-04 19:39:49 -0300

    cria reducer para os posts

---
+++ reddit/src/reducers/posts.js
@@ -0,0 +1,33 @@
+import { GET_REDDIT_POSTS, GET_REDDIT_POSTS_ERROR, GET_REDDIT_POSTS_SUCCESS } from '../actions/index';
+
+const INITIAL_STATE = {
+  posts: [],
+  loading: false,
+  error: null,
+}
+
+const postsReducer = (state = INITIAL_STATE, action) => {
+  switch(action.type) {
+    case GET_REDDIT_POSTS:
+      return {
+        ...state,
+        loading: true,
+      }
+    case GET_REDDIT_POSTS_SUCCESS:
+      return {
+        ...state,
+        posts: action.payload,
+        loading: false,
+      }
+    case GET_REDDIT_POSTS_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      }
+    default:
+      return state;
+  }
+}
+
+export default postsReducer;
